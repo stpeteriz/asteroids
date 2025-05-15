c533aeaac42cba5295ca6b96f06a0285f7a18ddd
JohnStpeter <st.peteriz17@gmail.com>
2025-05-15 19:59:10 +0200

    fourth commit - moving up n down and rotating right n left

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
+    player1 = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock= pygame.time.Clock()
@@ -13,8 +16,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player1.update(dt)
         screen.fill("black")
-        pygame.display.flip()
+        player1.draw(screen)
+      
+        
+        pygame.display.flip() 
+       
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
